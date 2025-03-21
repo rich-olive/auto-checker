25153d53f956e3a6d2a6852e42df11b8f34e02e5
olivia <oliviam.richmond@gmail.com>
2025-03-21 13:46:32 +0000

    created new project auto checker

---
+++ autochecker.py
@@ -0,0 +1,13 @@
+# open an excel spreadsheet
+
+# later this can be done using user input and a path
+
+# cycle through each column
+
+# check content against criteria, using logic
+
+# logic including regex checking for forbidden characters, certain characters, whitespace
+# check for length
+
+import openpyxl
+
